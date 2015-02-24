843700b22293685f0d7c3e61a7c95502bfc1ffca
HrabiaVulpes <hrabiavulpes@gmail.com>
2015-02-24 19:03:15 +0100

    dettach, do tego troche komentarzy i w ogóle drobna zmiana w ojcu wszystkich filtrów

--- src/Filter.java
+++ src/Filter.java
@@ -2,15 +2,17 @@
  * Created by vulpes on 21.02.15.
  */
 public class Filter {
-    Filter next = null;
+    Filter next = null;                                     // następny po nim
     void attach(Filter next_module){
         next = next_module;
-    }
+    } // doczep następny po nim
+    void dettach() { if (next != null) next = next.next;}   // odczep następny po nim
     Filter(Filter next_module){
         next = next_module;
-    }
-    Filter() {next = null; };
-    Message handle(Message input){
+    }      // twórz z od razu doczepionym następnym
+    Filter() {next = null; };                               // twórz bez doczepionego
+    Message handle(Message input){                          // przetwórz wiadomość
+        if(next != null) return next.handle(input);
         return input;
     }
 }
